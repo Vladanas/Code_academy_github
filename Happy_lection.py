96f951982686ea9faf04ce6a766efaaf5c08cda2
Vladanas <transekspress.vladislav@gmail.com>
2023-12-19 21:40:13 +0200

    Kodo papildimas

--- Happy_lection.py
+++ Happy_lection.py
@@ -11,3 +11,35 @@ book1 = Book("Programavimo ABC", "John Smith", True)
 print(f"Knygos pavadinimas: {book1.title}")
 print(f"Autorius: {book1.author}")
 print(f"Ar knyga prieinama: {book1.available}")
+
+
+                            # Exercizes 3
+                            # Task 1
+
+class Book:
+    def __init__(self, title, author, available):
+        self.title = title
+        self.author = author
+        self.available = available
+
+    def read_book(self):
+        if self.available:
+            print(f"Jūs skaitote knygą '{self.title}'")
+        else:
+            print("Atsiprašome, ši knyga šiuo metu neprieinama")
+
+    def change_availability(self, new_status):
+        self.available = new_status
+        if new_status:
+            print(f"Knyga '{self.title}' dabar prieinama")
+        else:
+            print(f"Knyga '{self.title}' dabar neprieinama")
+
+# Sukuriam knygos objektą
+book1 = Book("Programavimo ABC", "John Smith", True)
+
+# Išbandome metodus
+book1.read_book()  # Rezultatas: "Jūs skaitote knygą 'Programavimo ABC'"
+book1.change_availability(False)  # Rezultatas: "Knyga 'Programavimo ABC' dabar neprieinama"
+book1.read_book()  # Rezultatas: "Atsiprašome, ši knyga šiuo metu neprieinama"
+
